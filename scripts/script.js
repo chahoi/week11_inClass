121bc28b56e72a406f25d24924a5ffaece678ef5
Jake Choi <9822cj1@gmail.com>
2022-11-15 09:46:48 -0800

    practice function in three different ways

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// function sleepy (name) {
+//     /* let my_status = "I am sleepy as fuck" */
+//     const my_name = name
+//     console.log({my_name} + 'is sleepy as ')
+//     prompt({my_name} + "is tired")
+// }
+
+// let userInput = prompt("write your name: ")
+// sleepy (userInput)
+
+
+// function greet (username){
+//     console.log(username);
+// }
+
+// greet("Jake")
+
+let greet = (userName) => {
+    console.log(`${userName} Hello`)
+}
+
+greet("Jake")
+greet("Nigel")
